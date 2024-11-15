475a9f0dfe1fe87192648559e8fcde52ccd118db
Tasia <fta2019@yandex.ru>
2024-11-15 13:10:53 +0300

    Changes have been added to the solution files

--- calculate.py
+++ calculate.py
@@ -1,33 +1,52 @@
 import circle
 import square
+import triangle
 
 
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+    'circle-area': 1,
+    'circle-perimeter': 1,
+    'square-area': 1,
+    'square-perimeter': 1,
+    'triangle-area': 3,
+    'triangle-perimeter': 3
+}
+
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+    assert fig in figs
+    assert func in funcs
+    key = f'{fig}-{func}'
+    expected_args = sizes.get(key)
+    assert expected_args is not None
+    assert len(size) == expected_args
+    assert all(s >= 0 for s in size)
+    if fig == 'triangle':
+        a, b, c = size
+        assert a + b > c and a + c > b and b + c > a
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+    result = eval(f'{fig}.{func}(*{size})')
+    return result
+
 
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
+    func = ''
+    fig = ''
+    size = list()
+
+    while fig not in figs:
+        fig = input(f"Enter figure name, avaliable are {figs}:\n")
+
+    while func not in funcs:
+        func = input(f"Enter function name, avaliable are {funcs}:\n")
+
+    while len(size) != sizes.get(f"{func}-{fig}", 1):
+        size = list(map(int, input(
+            "Input figure sizes separated by space, 1 for circle and square\n"
+        ).split(' ')))
+
+    calc(fig, func, size)
+
     
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
-
-
-
--- triangle.py
+++ triangle.py
@@ -1,6 +1,12 @@
+import math
+
+
 def area(a, b, c):
-    return (a + b + c) / 2
+    s = (a + b + c) / 2
+    return math.sqrt(s * (s - a) * (s - b) * (s - c))
 
-
+    
 def perimeter(a, b, c):
     return a + b + c
+
+
